ded274ec50b93256a2d26b732df224add7c68ce5
yadavswapnil309@gmail.com <yadavswapnil309@gmail.com>
2025-06-11 15:46:58 +0530

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import Navbar from './components/Navbar';
-import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import React from "react";
+import Login from "./components/Login";
+import Body from "./components/Body";
+import Profile from "./components/Profile";
+
 function App() {
-return(
-  <div>
-    <Navbar/>
-  </div>
-)
+  return (
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
